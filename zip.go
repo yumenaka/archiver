31d516047dcdb5d0ce86f25f9a422131c3fdfa7a
yumenaka <www.bailin.tv@gmail.com>
2021-12-05 14:48:21 +0800

    Fix Bug

--- zip.go
+++ zip.go
@@ -89,7 +89,7 @@ type Zip struct {
 
 // CheckExt ensures the file extension matches the format.
 func (*Zip) CheckExt(filename string) error {
-	if !(strings.HasSuffix(filename, ".zip") && strings.HasSuffix(filename, ".cbz")) {
+	if !(strings.HasSuffix(filename, ".zip") || strings.HasSuffix(filename, ".cbz")) {
 		return fmt.Errorf("filename must have a .zip or .cbz extension")
 	}
 	return nil
